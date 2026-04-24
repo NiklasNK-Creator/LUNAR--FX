656d86a4e29d1194881905d20403f75589796308
NiklasNK-Creator <schoneniklas79@gmail.com>
2026-04-24 23:09:50 +0200

    Release v1.1.6

--- Source/LUNAR_IMP/LUNAR_IMP.cpp
+++ Source/LUNAR_IMP/LUNAR_IMP.cpp
@@ -90,12 +90,9 @@ ParamsSetup (
 	
 	AEFX_CLR_STRUCT(def);
 	
-	strcpy_s(def.name, sizeof(def.name), "Impact Frame");
-	PF_ADD_CHECKBOX(def.name, FALSE, IMPACT_FRAME_DISK_ID, 0, 0);
+	PF_ADD_CHECKBOX("Impact Frame", FALSE, IMPACT_FRAME_DISK_ID, 0, 0);
 	
-	AEFX_CLR_STRUCT(def);
-	strcpy_s(def.name, sizeof(def.name), "Hold");
-	PF_ADD_CHECKBOX(def.name, FALSE, HOLD_DISK_ID, 0, 0);
+	PF_ADD_CHECKBOX("Hold", FALSE, HOLD_DISK_ID, 0, 0);
 	
 	PF_ADD_FLOAT_SLIDERX("Radius", 
 						RADIUS_MIN,
